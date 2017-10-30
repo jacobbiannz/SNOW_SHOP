fd52617610e854b3122976552ecf2159f186873d
boChuck <bo.rubyxing@gmail.com>
2017-10-30 21:59:30 +1300

    update

--- SNOW.SHOP.API/API/ViewModel/CategoryViewModel.cs
+++ SNOW.SHOP.API/API/ViewModel/CategoryViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
 
 namespace SNOW.SHOP.API.API.ViewModel
@@ -11,8 +12,10 @@ namespace SNOW.SHOP.API.API.ViewModel
 
         public String Name { get; set; }
 
+        [IgnoreDataMember]
         public CompanyViewModel Company { get; set; }
 
+        [IgnoreDataMember]
         public ICollection<KeyValuePair<string, string>> AllProducts { get; set; }
     }
 }
